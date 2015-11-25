12664eb245177e8f44aaec6052a4a83839af5ab6
Billy Nicart <bnicart@you-source.com>
2015-11-25 12:12:24 +0800

    [Supplied code to passport.js]

---
+++ config/passport.js
@@ -0,0 +1,17 @@
+var JwtStrategy = require('passport-jwt').Strategy;
+
+var User = require('../app/models/User');
+var config = require('../config/database');
+
+module.exports = function(passport) {
+  var opts = {};
+  opts.secretKey = config.secret;
+  passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    User.findOne({id: jwt_payload.id}, function (err, user) {
+      if (err) { return done(err, false); }
+      if (user) { done(null, user); }
+      else { done(null, false); }
+    });
+  }));
+};
+
